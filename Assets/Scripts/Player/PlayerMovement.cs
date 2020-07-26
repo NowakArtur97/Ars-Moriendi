bdb2b257f5f50cea5885a5613dad1568b6d46335
NowakArtur97 <nowart97@gmail.com>
2020-07-26 09:23:21 +0200

    Add Jumping to Player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,16 +2,25 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private readonly float runSpeed = 5f;
+    [SerializeField] private float runSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 5f;
+    [SerializeField] private LayerMask ground;
 
     private InputMaster controls;
 
     private Rigidbody2D myRigidbody2D;
+    private Collider2D myCollider2D;
 
     private void Awake()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        myCollider2D = GetComponent<Collider2D>();
         controls = new InputMaster();
+    }
+
+    private void Start()
+    {
+        controls.Player.Jump.performed += _ => Jump();
     }
 
     private void Update()
@@ -25,6 +34,27 @@ public class PlayerMovement : MonoBehaviour
         Vector3 currentPosition = transform.position;
         currentPosition.x += movementInput * runSpeed * Time.deltaTime;
         transform.position = currentPosition;
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            myRigidbody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector2 topLeftPoint = transform.position;
+        topLeftPoint.x -= myCollider2D.bounds.extents.x;
+        topLeftPoint.y += myCollider2D.bounds.extents.y;
+
+        Vector2 bottomRightPoint = transform.position;
+        bottomRightPoint.x += myCollider2D.bounds.extents.x;
+        bottomRightPoint.y -= myCollider2D.bounds.extents.y;
+
+        return Physics2D.OverlapArea(topLeftPoint, bottomRightPoint, ground);
     }
 
     private void OnEnable()
