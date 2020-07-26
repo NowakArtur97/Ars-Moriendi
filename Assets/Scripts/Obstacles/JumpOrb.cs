2c85484de4f8b4bb3d376270ec3cf161207f4b7c
NowakArtur97 <nowart97@gmail.com>
2020-07-26 21:39:18 +0200

    Deactivate Jump Orb after one use for a short time

---
+++ Assets/Scripts/Obstacles/JumpOrb.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class JumpOrb : MonoBehaviour
+{
+    private float enableTimer = 5f;
+    private bool isActive;
+
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Awake()
+    {
+        isActive = true;
+
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (!isActive)
+        {
+            enableTimer -= Time.deltaTime;
+            if (enableTimer <= 0)
+            {
+                ActivateOrb();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isActive && collision.gameObject.tag.Equals("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().TouchedJumpOrb();
+
+            DectivateOrb();
+        }
+    }
+
+    private void ActivateOrb()
+    {
+        isActive = true;
+        mySpriteRenderer.color = new Color(0, 49, 255, 1f);
+    }
+
+    private void DectivateOrb()
+    {
+        isActive = false;
+        enableTimer = 5f;
+        mySpriteRenderer.color = new Color(0, 49, 255, 0.5f);
+    }
+}
