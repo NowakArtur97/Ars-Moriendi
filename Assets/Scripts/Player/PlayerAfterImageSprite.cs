bc100fbd6436b6ea43d35e4dec4df2bcb4f61340
NowakArtur97 <nowart97@gmail.com>
2020-08-26 14:34:59 +0200

    Create Player After Image Sprite script and prefab

---
+++ Assets/Scripts/Player/PlayerAfterImageSprite.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerAfterImageSprite : MonoBehaviour
+{
+    [SerializeField] private float activeTime = 0.1f;
+    private float fadingTimer;
+    [SerializeField] private float defaultAlpha = 0.8f;
+    private float curentAlpha;
+    [SerializeField] private float alphaMultiplier = 0.85f;
+
+    private Transform playerPosition;
+    private SpriteRenderer playerSpriteRenderer;
+
+    private Color spriteColor;
+    private SpriteRenderer mySpriteRenderer;
+
+    private void OnEnable()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player").gameObject;
+        playerPosition = player.transform;
+        playerSpriteRenderer = player.GetComponentInChildren<SpriteRenderer>();
+
+        transform.position = playerPosition.position;
+        transform.rotation = playerPosition.rotation;
+
+        mySpriteRenderer.sprite = playerSpriteRenderer.sprite;
+
+        curentAlpha = defaultAlpha;
+        fadingTimer = Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        curentAlpha *= alphaMultiplier;
+        spriteColor = new Color(1, 1, 1, curentAlpha);
+        mySpriteRenderer.color = spriteColor;
+
+        if (fadingTimer + activeTime > Time.deltaTime)
+        {
+
+        }
+    }
+}
