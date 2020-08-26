b7b7815b6b73ec51b5e4f3354428a3295f300a1c
NowakArtur97 <nowart97@gmail.com>
2020-08-26 14:47:02 +0200

    Create Player After Image Pool script

---
+++ Assets/Scripts/Player/after image/PlayerAfterImagePool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAfterImagePool : MonoBehaviour
+{
+    [SerializeField] private GameObject playerAfterImagePrefab;
+
+    public static PlayerAfterImagePool instance { get; private set; }
+    private Queue<GameObject> activeObjects = new Queue<GameObject>();
+
+    private void Awake()
+    {
+        instance = this;
+        GrowPool();
+    }
+
+    private void GrowPool()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject instanceToAdd = Instantiate(playerAfterImagePrefab, transform);
+
+            AddToPool(instanceToAdd);
+        }
+    }
+
+    public void AddToPool(GameObject instance)
+    {
+        instance.SetActive(false);
+
+        activeObjects.Enqueue(instance);
+    }
+
+    public GameObject GetFromPool()
+    {
+        if (activeObjects.Count == 0)
+        {
+            GrowPool();
+        }
+
+        GameObject instance = activeObjects.Dequeue();
+        instance.SetActive(true);
+
+        return instance;
+    }
+}
