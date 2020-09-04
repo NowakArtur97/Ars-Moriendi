0d26234e3ea19960d8dd6b29453a601720b565c1
NowakArtur97 <nowart97@gmail.com>
2020-09-04 16:18:30 +0200

    Create Basic Enemy Controller and setup basic structure and methods

---
+++ Assets/Scripts/Enemy/BasicEnemyController.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour
+{
+    private State currentState;
+
+    private enum State
+    {
+        Moving, Knockback, Dead
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case State.Moving:
+                UpdateMovingState();
+                break;
+            case State.Knockback:
+                UpdateKnockbackState();
+                break;
+            case State.Dead:
+                UpdateDeadState();
+                break;
+        }
+    }
+
+    #region Moving State
+    private void EnterMovingState()
+    {
+
+    }
+
+    private void UpdateMovingState()
+    {
+
+    }
+
+    private void ExitMovingState()
+    {
+
+    }
+    #endregion
+
+    #region Knockback State
+    private void EnterKnockbackState()
+    {
+
+    }
+
+    private void UpdateKnockbackState()
+    {
+
+    }
+
+    private void ExitKnockbackState()
+    {
+
+    }
+    #endregion
+
+    #region Dead State
+    private void EnterDeadState()
+    {
+
+    }
+
+    private void UpdateDeadState()
+    {
+
+    }
+
+    private void ExitDeadState()
+    {
+
+    }
+    #endregion
+
+    #region Other
+    private void ChangeState(State newState)
+    {
+        switch (currentState)
+        {
+            case State.Moving:
+                ExitMovingState();
+                break;
+            case State.Knockback:
+                ExitKnockbackState();
+                break;
+            case State.Dead:
+                ExitDeadState();
+                break;
+        }
+
+        switch (newState)
+        {
+            case State.Moving:
+                EnterMovingState();
+                break;
+            case State.Knockback:
+                EnterKnockbackState();
+                break;
+            case State.Dead:
+                EnterDeadState();
+                break;
+        }
+
+        currentState = newState;
+    }
+    #endregion
+}
