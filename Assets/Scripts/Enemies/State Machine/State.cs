ea70439654bb1d469a9c3693c95d908a1ba2cd2e
NowakArtur97 <nowart97@gmail.com>
2020-09-08 14:42:38 +0200

    Create basic State script

---
+++ Assets/Scripts/Enemies/State Machine/State.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class State
+{
+    protected FiniteStateMachine finiteStateMachine;
+    protected Entity entity;
+
+    protected float startTime;
+
+    public State(FiniteStateMachine finiteStateMachine, Entity entity)
+    {
+        this.finiteStateMachine = finiteStateMachine;
+        this.entity = entity;
+    }
+
+    public virtual void Enter()
+    {
+        startTime = Time.time;
+    }
+
+    public virtual void Exit()
+    {
+    }
+
+    public virtual void LogicUpdateFunction()
+    {
+    }
+
+    public virtual void PhysicsUpdateFunction()
+    {
+    }
+}
