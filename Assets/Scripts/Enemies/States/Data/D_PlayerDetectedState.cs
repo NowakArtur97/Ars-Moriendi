7e74a0644a1cb0095029ff0dde9192c1bdc0b601
NowakArtur97 <nowart97@gmail.com>
2020-09-08 17:15:47 +0200

    Create Player Detected State scriptable object

---
+++ Assets/Scripts/Enemies/States/Data/D_PlayerDetectedState.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player Detected State Data", menuName = "Data/State Data/Player Detected State")]
+public class D_PlayerDetectedState : ScriptableObject
+{
+    public float playerDetectionRange = 3f;
+}
