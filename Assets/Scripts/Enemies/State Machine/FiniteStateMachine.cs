0dc5694cc0aa65415d87063c7be6db39d52b3102
NowakArtur97 <nowart97@gmail.com>
2020-09-08 14:46:27 +0200

    Create Finite State Machine script

---
+++ Assets/Scripts/Enemies/State Machine/FiniteStateMachine.cs
@@ -0,0 +1,17 @@
+﻿public class FiniteStateMachine
+{
+    public State currentState { get; private set; }
+
+    public void Initialize(State startingState)
+    {
+        currentState = startingState;
+        currentState.Enter();
+    }
+
+    public void ChangeState(State newState)
+    {
+        currentState.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+}
