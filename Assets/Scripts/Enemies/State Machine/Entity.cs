1edcacdc5e3509d3cff81a68b5a9379e5404b407
NowakArtur97 <nowart97@gmail.com>
2020-09-08 14:58:24 +0200

    Create Entity script

---
+++ Assets/Scripts/Enemies/State Machine/Entity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody2D { get; private set; }
+    public Animator myAnimator { get; private set; }
+    public GameObject aliveGameObject { get; private set; }
+
+    public virtual void Awake()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        aliveGameObject = transform.Find("Alive").gameObject;
+    }
+}
--- Assets/Scripts/Enemies/State Machine/State.cs
+++ Assets/Scripts/Enemies/State Machine/State.cs
@@ -4,22 +4,27 @@ public class State
 {
     protected FiniteStateMachine finiteStateMachine;
     protected Entity entity;
+    protected string animationBoolName;
 
     protected float startTime;
 
-    public State(FiniteStateMachine finiteStateMachine, Entity entity)
+    public State(FiniteStateMachine finiteStateMachine, Entity entity, string animationBoolName)
     {
         this.finiteStateMachine = finiteStateMachine;
         this.entity = entity;
+        this.animationBoolName = animationBoolName;
     }
 
     public virtual void Enter()
     {
         startTime = Time.time;
+
+        entity.myAnimator.SetBool(animationBoolName, true);
     }
 
     public virtual void Exit()
     {
+        entity.myAnimator.SetBool(animationBoolName, false);
     }
 
     public virtual void LogicUpdateFunction()
