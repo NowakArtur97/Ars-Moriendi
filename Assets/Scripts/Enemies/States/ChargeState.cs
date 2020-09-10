564f5a7d4231ba506fed1fda3d7531366ed49c7d
NowakArtur97 <nowart97@gmail.com>
2020-09-10 15:13:29 +0200

    Create Charge State scriptable object and setup basic Charge State

---
+++ Assets/Scripts/Enemies/States/ChargeState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeState : State
+{
+    protected D_ChargeState stateData;
+
+    public ChargeState(FiniteStateMachine finiteStateMachine, Entity entity, string animationBoolName, D_ChargeState stateData)
+        : base(finiteStateMachine, entity, animationBoolName)
+    {
+        this.stateData = stateData;
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdateFunction()
+    {
+        base.LogicUpdateFunction();
+    }
+
+    public override void PhysicsUpdateFunction()
+    {
+        base.PhysicsUpdateFunction();
+    }
+}
---
+++ Assets/Scripts/Enemies/States/Data/D_ChargeState.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Charge State Data", menuName = "Data/State Data/Charge State")]
+public class D_ChargeState : ScriptableObject
+{
+    public float chargeSpeed = 4f;
+}
