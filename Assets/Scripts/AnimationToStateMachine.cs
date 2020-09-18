b387069e5a530eeb1d45d0b9cf215df871cf20c0
NowakArtur97 <nowart97@gmail.com>
2020-09-18 17:50:46 +0200

    Create Animation To State Machine for sending actions from animation controller to object

---
+++ Assets/Scripts/AnimationToStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AnimationToStateMachine : MonoBehaviour
+{
+    public AttackState attackState;
+
+    private void TriggerAttack()
+    {
+        attackState.TriggerAttack();
+    }
+
+    private void FinishAttack()
+    {
+        attackState.FinishAttack();
+    }
+}
