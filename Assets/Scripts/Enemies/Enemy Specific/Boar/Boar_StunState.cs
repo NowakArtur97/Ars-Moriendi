bee675e11f364fd4fd53686f41dc54cea955c90e
NowakArtur97 <nowart97@gmail.com>
2020-09-19 17:24:51 +0200

    Create basic Boar Stun State

---
+++ Assets/Scripts/Enemies/Enemy Specific/Boar/Boar_StunState.cs
@@ -0,0 +1,35 @@
+﻿public class Boar_StunState : StunState
+{
+    private Boar boar;
+
+    public Boar_StunState(FiniteStateMachine finiteStateMachine, Entity entity, string animationBoolName, D_StunState stateData, Boar boar)
+        : base(finiteStateMachine, entity, animationBoolName, stateData)
+    {
+        this.boar = boar;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdateFunction()
+    {
+        base.LogicUpdateFunction();
+    }
+
+    public override void PhysicsUpdateFunction()
+    {
+        base.PhysicsUpdateFunction();
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+}
