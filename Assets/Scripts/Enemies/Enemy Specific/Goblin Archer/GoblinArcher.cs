4e814693207933386327ca51643ea1bf6bb90fca
NowakArtur97 <nowart97@gmail.com>
2020-09-23 17:31:12 +0200

    Create Goblin Archer Idle state

--- Assets/Scripts/Enemies/Enemy Specific/Goblin Archer/GoblinArcher.cs
+++ Assets/Scripts/Enemies/Enemy Specific/Goblin Archer/GoblinArcher.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GoblinArcher : MonoBehaviour
+public class GoblinArcher : Entity
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("States Data")]
+    [SerializeField] private D_IdleState idleStateData;
+    [SerializeField] private D_MoveState moveStateData;
+
+    public GoblinArcher_IdleState idleState { get; private set; }
+    public GoblinArcher_MoveState moveState { get; private set; }
+
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Damage(AttackDetails attackDetails)
     {
-        
+        base.Damage(attackDetails);
+
+        idleState = new GoblinArcher_IdleState(finiteStateMachine, this, "idle", idleStateData, this);
+        moveState = new GoblinArcher_MoveState(finiteStateMachine, this, "move", moveStateData, this);
     }
 }
---
+++ Assets/Scripts/Enemies/Enemy Specific/Goblin Archer/GoblinArcher_IdleState.cs
@@ -0,0 +1,45 @@
+﻿public class GoblinArcher_IdleState : IdleState
+{
+    private GoblinArcher goblinArcher;
+
+    public GoblinArcher_IdleState(FiniteStateMachine finiteStateMachine, Entity entity, string animationBoolName, D_IdleState stateData, GoblinArcher goblinArcher)
+        : base(finiteStateMachine, entity, animationBoolName, stateData)
+    {
+        this.goblinArcher = goblinArcher;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdateFunction()
+    {
+        base.LogicUpdateFunction();
+
+        if (isPlayerInMinAgroRange || isPlayerInMaxAgroRange)
+        {
+            // TODO: Player detected state
+            //finiteStateMachine.ChangeState(goblinArcher.playerDetectedState);
+        }
+        else if (isIdleTimeOver)
+        {
+            finiteStateMachine.ChangeState(goblinArcher.moveState);
+        }
+    }
+
+    public override void PhysicsUpdateFunction()
+    {
+        base.PhysicsUpdateFunction();
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+}
