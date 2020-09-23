f38396dcac7df38cbf578d7515fa2533c543d8a2
NowakArtur97 <nowart97@gmail.com>
2020-09-23 17:33:26 +0200

    Create Goblin Archer Move State

--- Assets/Scripts/Enemies/Enemy Specific/Goblin Archer/GoblinArcher_MoveState.cs
+++ Assets/Scripts/Enemies/Enemy Specific/Goblin Archer/GoblinArcher_MoveState.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GoblinArcher_MoveState : MonoBehaviour
+﻿public class GoblinArcher_MoveState : MoveState
 {
-    // Start is called before the first frame update
-    void Start()
+    private GoblinArcher goblinArcher;
+
+    public GoblinArcher_MoveState(FiniteStateMachine finiteStateMachine, Entity entity, string animationBoolName, D_MoveState stateData, GoblinArcher goblinArcher)
+        : base(finiteStateMachine, entity, animationBoolName, stateData)
     {
-        
+        this.goblinArcher = goblinArcher;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Enter()
     {
-        
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdateFunction()
+    {
+        base.LogicUpdateFunction();
+
+        if (isPlayerInMinAgroRange || isPlayerInMaxAgroRange)
+        {
+            //TODO: Player detected state
+            //finiteStateMachine.ChangeState(goblinArcher.playerDetectedState);
+        }
+        else if (!isDetectingLedge || isDetectingWall)
+        {
+            goblinArcher.idleState.SetFlipAfterIdle(true);
+            finiteStateMachine.ChangeState(goblinArcher.idleState);
+        }
+    }
+
+    public override void PhysicsUpdateFunction()
+    {
+        base.PhysicsUpdateFunction();
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
     }
 }
