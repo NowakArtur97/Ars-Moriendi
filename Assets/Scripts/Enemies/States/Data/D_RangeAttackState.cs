1a15f6b465aa53419d262f8e8cc29629f2b61588
NowakArtur97 <nowart97@gmail.com>
2020-09-24 17:05:40 +0200

    Create Projectile script

--- Assets/Scripts/Enemies/States/Data/D_RangeAttackState.cs
+++ Assets/Scripts/Enemies/States/Data/D_RangeAttackState.cs
@@ -3,5 +3,8 @@
 [CreateAssetMenu(fileName = "New Range Attack State Data", menuName = "Data/State Data/Range Attack State")]
 public class D_RangeAttackState : ScriptableObject
 {
-
+    public GameObject projectile;
+    public float projectileDamage = 10.0f;
+    public float projectileSpeed = 12.0f;
+    public float projectileTravelDistance = 8.0f;
 }
---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float gravity = 4.0f;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private LayerMask whatIsPlayer;
+    [SerializeField]
+    private Transform damagePosition;
+    [SerializeField]
+    private float damageRadious = 0.15f;
+
+    private AttackDetails attackDetails;
+
+    private float speed;
+    private float travelDistance;
+    private float xStartPosition;
+
+    private bool isGravityOn;
+    private bool hasHitGround;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myRigidbody2D.gravityScale = 0.0f;
+
+        myRigidbody2D.velocity = transform.right * speed;
+
+        xStartPosition = transform.position.x;
+
+        isGravityOn = false;
+        hasHitGround = false;
+    }
+
+    private void Update()
+    {
+        if (!hasHitGround)
+        {
+            attackDetails.position = transform.position;
+
+            if (isGravityOn)
+            {
+                float angle = Mathf.Atan2(myRigidbody2D.velocity.y, myRigidbody2D.velocity.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!hasHitGround)
+        {
+            Collider2D damageHit = Physics2D.OverlapCircle(damagePosition.position, damageRadious, whatIsPlayer);
+            Collider2D groundHit = Physics2D.OverlapCircle(damagePosition.position, damageRadious, whatIsGround);
+
+            if (damageHit)
+            {
+                damageHit.transform.SendMessage("Damage", attackDetails);
+                Destroy(gameObject);
+            }
+
+            if (groundHit)
+            {
+                hasHitGround = true;
+                myRigidbody2D.gravityScale = 0.0f;
+                myRigidbody2D.velocity = Vector2.zero;
+            }
+
+            if (Mathf.Abs(xStartPosition - transform.position.x) >= travelDistance && !isGravityOn)
+            {
+                isGravityOn = true;
+                myRigidbody2D.gravityScale = gravity;
+            }
+        }
+    }
+
+    public void FireProjectile(float speed, float travelDistance, float damage)
+    {
+        this.speed = speed;
+        this.travelDistance = travelDistance;
+        this.attackDetails.damageAmmount = damage;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(damagePosition.position, damageRadious);
+    }
+}
