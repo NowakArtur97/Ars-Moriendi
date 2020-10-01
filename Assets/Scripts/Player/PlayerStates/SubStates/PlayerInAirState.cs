5dc6721042d74b9c97e4cf9807d046642d417313
NowakArtur97 <nowart97@gmail.com>
2020-10-01 19:37:53 +0200

    Setup movement in Player In Air state

--- Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
@@ -1,6 +1,49 @@
 ﻿public class PlayerInAirState : PlayerState
 {
+    private int _xInput;
+
+    private bool _isGrounded;
+
     public PlayerInAirState(Player player, PlayerFiniteStateMachine PlayerFiniteStateMachine, D_PlayerData PlayerData, string animationBoolName) : base(player, PlayerFiniteStateMachine, PlayerData, animationBoolName)
     {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        _xInput = Player.InputHandler.NormalizedInputX;
+
+        if (_isGrounded & Player.CurrentVelocity.y < 0.01f)
+        {
+            PlayerFiniteStateMachine.ChangeState(Player.LandState);
+        }
+        else
+        {
+            Player.CheckIfShouldFlip(_xInput);
+            Player.SetVelocityX(PlayerData.movementVelocity * _xInput);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+
+        _isGrounded = Player.CheckIfGrounded();
     }
 }
