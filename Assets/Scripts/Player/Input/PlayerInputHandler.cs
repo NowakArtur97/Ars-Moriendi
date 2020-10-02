d12c9790e3b84abdd92636c2e458d2ad729792b0
NowakArtur97 <nowart97@gmail.com>
2020-10-02 18:17:32 +0200

    Add the ability to jump the Player before landing

--- Assets/Scripts/Player/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -3,10 +3,20 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    [SerializeField]
+    private float _inputHoldTime = 0.2f;
+
+    private float _jumpInputStartTime;
+
     public Vector2 RawMovementInput { get; private set; }
     public int NormalizedInputX { get; private set; }
     public int NormalizedInputY { get; private set; }
     public bool JumpInput { get; private set; }
+
+    private void Update()
+    {
+        CheckJumpInputHoldTime();
+    }
 
     public void OnMoveInput(CallbackContext context)
     {
@@ -21,8 +31,17 @@ public class PlayerInputHandler : MonoBehaviour
         if (context.started)
         {
             JumpInput = true;
+            _jumpInputStartTime = Time.time;
         }
     }
 
     public void UseJumpInput() => JumpInput = false;
+
+    public void CheckJumpInputHoldTime()
+    {
+        if (Time.time >= _jumpInputStartTime + _inputHoldTime)
+        {
+            JumpInput = false;
+        }
+    }
 }
