fdf570b2272529eae27795e7a2fc72ac9be139b4
NowakArtur97 <nowart97@gmail.com>
2020-10-15 17:27:50 +0200

    Create Basic Player Crouch Idle and Move states

---
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerCrouchIdleState.cs
@@ -0,0 +1,6 @@
+﻿public class PlayerCrouchIdleState : PlayerGroundedState
+{
+    public PlayerCrouchIdleState(Player player, PlayerFiniteStateMachine playerFiniteStateMachine, D_PlayerData playerData, string animationBoolName) : base(player, playerFiniteStateMachine, playerData, animationBoolName)
+    {
+    }
+}
