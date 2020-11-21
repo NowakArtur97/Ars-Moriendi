36cafd5ad76c05d5d2257ae377a11e86e3c3c24e
NowakArtur97 <nowart97@gmail.com>
2020-11-21 18:00:02 +0100

    Setup On Rope Aim state

--- Assets/Scripts/Player/Player States/SubStates/PlayerOnRopeState.cs
+++ Assets/Scripts/Player/Player States/SubStates/PlayerOnRopeState.cs
@@ -4,9 +4,54 @@ using UnityEngine;
 
 public class PlayerOnRopeState : PlayerAbilityState
 {
+    private int _xInput;
+    private bool _ropeInputStop;
+    private bool _ropeAttached;
+    private int _clickCount;
+
+    protected Vector2 PlayerPosition;
+
     public PlayerOnRopeState(Player player, PlayerFiniteStateMachine playerFiniteStateMachine, D_PlayerData playerData, string animationBoolName) : base(player, playerFiniteStateMachine, playerData, animationBoolName)
     {
     }
 
+    public override void Enter()
+    {
+        base.Enter();
+    }
 
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (!IsExitingState)
+        {
+            _ropeInputStop = Player.InputHandler.SecondaryAttackInputStop;
+
+            if (!_ropeAttached && !_ropeInputStop)
+            {
+                Player.FiniteStateMachine.ChangeState(Player.OnRopeStateAim);
+            }
+            //else if (!_ropeAttached && _clickCount == 1)
+            //{
+            //    Player.InputHandler.UseSecondaryAttackInputStop();
+
+            //    AttachRope();
+            //}
+            //else if (_ropeAttached && _clickCount == 1)
+            //{
+            //    Player.CheckIfShouldFlip(_xInput);
+
+            //    UpdateRopePositions();
+            //}
+            //else if (_ropeAttached && _clickCount == 2)
+            //{
+            //    Player.InputHandler.UseSecondaryAttackInputStop();
+
+            //    ResetRope();
+
+            //    IsAbilityDone = true;
+            //}
+        }
+    }
 }
--- Assets/Scripts/Player/Player States/SubStates/PlayerOnRopeState_Aim.cs
+++ Assets/Scripts/Player/Player States/SubStates/PlayerOnRopeState_Aim.cs
@@ -4,7 +4,45 @@ using UnityEngine;
 
 public class PlayerOnRopeState_Aim : PlayerOnRopeState
 {
+    private Vector2 _ropeDirectionInput;
+    private Vector3 _crossHairPosition;
+    private Vector2 _aimDirection;
+    private float _aimAngle;
+
     public PlayerOnRopeState_Aim(Player player, PlayerFiniteStateMachine playerFiniteStateMachine, D_PlayerData playerData, string animationBoolName) : base(player, playerFiniteStateMachine, playerData, animationBoolName)
     {
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        PlayerPosition = Player.transform.position;
+        _ropeDirectionInput = Player.InputHandler.RawSecondaryAttackDirectionInput;
+        _aimAngle = Mathf.Atan2(_ropeDirectionInput.y, _ropeDirectionInput.x);
+
+        SetCrosshairPosition();
+    }
+
+    private void SetCrosshairPosition()
+    {
+        if (_aimAngle < 0f)
+        {
+            _aimAngle = Mathf.PI * 2 + _aimAngle;
+        }
+        _aimDirection = Quaternion.Euler(0, 0, _aimAngle * Mathf.Rad2Deg) * Vector2.right;
+
+        if (!Player.Crosshair.gameObject.activeInHierarchy)
+        {
+            Player.Crosshair.gameObject.SetActive(true);
+        }
+
+        Player.RopeHingeAnchor.gameObject.SetActive(false);
+
+        float x = PlayerPosition.x + PlayerData.ropeCrosshairOffset * Mathf.Cos(_aimAngle);
+        float y = PlayerPosition.y + PlayerData.ropeCrosshairOffset * Mathf.Sin(_aimAngle);
+
+        _crossHairPosition = new Vector3(x, y, 0);
+        Player.Crosshair.position = _crossHairPosition;
     }
 }
