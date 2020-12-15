1cff377355d9b068f77d36b98b9a963b2c6b2a51
NowakArtur97 <nowart97@gmail.com>
2020-12-15 07:58:38 +0100

    Change Player Crouch Idle state constructor

--- Assets/Player/Scripts/Player States/SubStates/PlayerGroundedState/PlayerCrouchIdleState.cs
+++ Assets/Player/Scripts/Player States/SubStates/PlayerGroundedState/PlayerCrouchIdleState.cs
@@ -1,7 +1,7 @@
 ﻿public class PlayerCrouchIdleState : PlayerGroundedState
 {
-    public PlayerCrouchIdleState(Player player, PlayerFiniteStateMachine playerFiniteStateMachine, D_PlayerData playerData,
-        string animationBoolName) : base(player, playerFiniteStateMachine, playerData, animationBoolName)
+    public PlayerCrouchIdleState(Player player, PlayerFiniteStateMachine playerFiniteStateMachine, string animationBoolName)
+        : base(player, playerFiniteStateMachine, animationBoolName)
     {
     }
 
