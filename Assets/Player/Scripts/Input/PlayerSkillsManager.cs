564d5c6c92c856cd7a7c3d1382a6f22a8734843d
NowakArtur97 <nowart97@gmail.com>
2020-12-17 18:01:22 +0100

    Create Player Skill Manager script

---
+++ Assets/Player/Scripts/Input/PlayerSkillsManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+public class PlayerSkillsManager
+{
+    private List<PlayerAbilityState> _skills;
+    private int _currentSkillIndex;
+
+    public PlayerSkillsManager()
+    {
+        _skills = new List<PlayerAbilityState>();
+        _currentSkillIndex = -1;
+    }
+
+    public void AddSkill(PlayerAbilityState newSkill)
+    {
+        _skills.Add(newSkill);
+        _currentSkillIndex++;
+    }
+
+    public PlayerAbilityState GetCurrentSkill()
+    {
+        return _skills[_currentSkillIndex];
+    }
+
+    public void ChangeSkillUp()
+    {
+        _currentSkillIndex++;
+        if (_currentSkillIndex >= _skills.Count)
+        {
+            _currentSkillIndex = 0;
+        }
+    }
+
+    public void ChangeSkillDown()
+    {
+        _currentSkillIndex--;
+        if (_currentSkillIndex >= _skills.Count)
+        {
+            _currentSkillIndex = 0;
+        }
+    }
+}
