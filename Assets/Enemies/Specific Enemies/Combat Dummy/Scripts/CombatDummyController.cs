2b0dd6a8d7443a512114fc0c4a6cfa7c497d7b40
NowakArtur97 <nowart97@gmail.com>
2020-12-21 08:39:55 +0100

    Refacotr Combat Dummy script

--- Assets/Enemies/Specific Enemies/Combat Dummy/Scripts/CombatDummyController.cs
+++ Assets/Enemies/Specific Enemies/Combat Dummy/Scripts/CombatDummyController.cs
@@ -3,67 +3,67 @@ using UnityEngine;
 
 public class CombatDummyController : MonoBehaviour
 {
-    [SerializeField] private bool isKnockBackEnabled = true;
-    [SerializeField] private float maxHealth = 50f;
-    private float healthLeft;
-    [SerializeField] private float knockBackXForce = 3f;
-    [SerializeField] private float knockBackYForce = 0f;
+    [SerializeField] private bool _isKnockBackEnabled = true;
+    [SerializeField] private float _maxHealth = 50f;
+    private float _healthLeft;
+    [SerializeField] private float _knockBackXForce = 3f;
+    [SerializeField] private float _knockBackYForce = 0f;
 
-    [SerializeField] private float deadTopXForce = 2f;
-    [SerializeField] private float deadTopYForce = 2f;
-    [SerializeField] private float deadBottomXForce = 1f;
-    [SerializeField] private float deadBottomYForce = 1f;
-    [SerializeField] private float deadShieldXForce = 2.5f;
-    [SerializeField] private float deadShieldYForce = 2.5f;
+    [SerializeField] private float _deadTopXForce = 2f;
+    [SerializeField] private float _deadTopYForce = 2f;
+    [SerializeField] private float _deadBottomXForce = 1f;
+    [SerializeField] private float _deadBottomYForce = 1f;
+    [SerializeField] private float _deadShieldXForce = 2.5f;
+    [SerializeField] private float _deadShieldYForce = 2.5f;
 
-    [SerializeField] private GameObject damagePartcileEffect;
+    [SerializeField] private GameObject _damagePartcileEffect;
 
-    private float playerFacingDirection;
-    private PlayerMovementController playerMovementController;
+    private float _playerFacingDirection;
+    private PlayerMovementController _playerMovementController;
 
-    private GameObject aliveGO;
-    private GameObject deadTopGO;
-    private GameObject deadBottomGO;
-    private GameObject deadShieldGO;
+    private GameObject _aliveGO;
+    private GameObject _deadTopGO;
+    private GameObject _deadBottomGO;
+    private GameObject _deadShieldGO;
 
-    private Rigidbody2D aliveRigidBody2D;
-    private Rigidbody2D deadTopRigidBody2D;
-    private Rigidbody2D deadBottomRigidBody2D;
-    private Rigidbody2D deadShieldRigidBody2D;
+    private Rigidbody2D _aliveRigidBody2D;
+    private Rigidbody2D _deadTopRigidBody2D;
+    private Rigidbody2D _deadBottomRigidBody2D;
+    private Rigidbody2D _deadShieldRigidBody2D;
 
-    private Animator aliveAnimator;
+    private Animator _aliveAnimator;
 
     private void Awake()
     {
-        healthLeft = maxHealth;
+        _healthLeft = _maxHealth;
     }
 
     private void Start()
     {
-        playerMovementController = GameObject.Find("Player").GetComponent<PlayerMovementController>();
+        _playerMovementController = GameObject.Find("Player").GetComponent<PlayerMovementController>();
 
-        aliveGO = transform.Find("Combat Dummy Alive").gameObject;
-        deadTopGO = transform.Find("Combat Dummy Dead Top").gameObject;
-        deadBottomGO = transform.Find("Combat Dummy Dead Bottom").gameObject;
-        deadShieldGO = transform.Find("Combat Dummy Dead Shield").gameObject;
+        _aliveGO = transform.Find("Combat Dummy Alive").gameObject;
+        _deadTopGO = transform.Find("Combat Dummy Dead Top").gameObject;
+        _deadBottomGO = transform.Find("Combat Dummy Dead Bottom").gameObject;
+        _deadShieldGO = transform.Find("Combat Dummy Dead Shield").gameObject;
 
-        aliveRigidBody2D = aliveGO.GetComponent<Rigidbody2D>();
-        deadTopRigidBody2D = deadTopGO.GetComponent<Rigidbody2D>();
-        deadBottomRigidBody2D = deadBottomGO.GetComponent<Rigidbody2D>();
-        deadShieldRigidBody2D = deadShieldGO.GetComponent<Rigidbody2D>();
+        _aliveRigidBody2D = _aliveGO.GetComponent<Rigidbody2D>();
+        _deadTopRigidBody2D = _deadTopGO.GetComponent<Rigidbody2D>();
+        _deadBottomRigidBody2D = _deadBottomGO.GetComponent<Rigidbody2D>();
+        _deadShieldRigidBody2D = _deadShieldGO.GetComponent<Rigidbody2D>();
 
-        aliveAnimator = aliveGO.GetComponent<Animator>();
+        _aliveAnimator = _aliveGO.GetComponent<Animator>();
     }
 
     private void Damage(AttackDetails attackDetails)
     {
-        healthLeft -= attackDetails.damageAmmount;
+        _healthLeft -= attackDetails.damageAmmount;
 
-        if (healthLeft <= 0)
+        if (_healthLeft <= 0)
         {
             Die();
         }
-        else if (isKnockBackEnabled)
+        else if (_isKnockBackEnabled)
         {
             SetAnimation(attackDetails.position.x);
             KnockBack();
@@ -73,36 +73,33 @@ public class CombatDummyController : MonoBehaviour
 
     private void InstantiateParticleEffect()
     {
-        Instantiate(damagePartcileEffect, aliveGO.transform.position, Quaternion.Euler(0, 0, UnityEngine.Random.Range(0f, 360f)));
+        Instantiate(_damagePartcileEffect, _aliveGO.transform.position, Quaternion.Euler(0, 0, UnityEngine.Random.Range(0f, 360f)));
     }
 
     private void SetAnimation(float xPosition)
     {
-        playerFacingDirection = xPosition > aliveGO.transform.position.x ? -1 : 1;
-        bool isPlayerOnTheLeft = playerFacingDirection == 1;
+        _playerFacingDirection = xPosition > _aliveGO.transform.position.x ? -1 : 1;
+        bool isPlayerOnTheLeft = _playerFacingDirection == 1;
 
-        aliveAnimator.SetBool("isPlayerOnTheLeft", isPlayerOnTheLeft);
-        aliveAnimator.SetTrigger("isDamaged");
+        _aliveAnimator.SetBool("isPlayerOnTheLeft", isPlayerOnTheLeft);
+        _aliveAnimator.SetTrigger("isDamaged");
     }
 
-    private void KnockBack()
-    {
-        aliveRigidBody2D.AddForce(new Vector2(knockBackXForce * playerFacingDirection, knockBackYForce), ForceMode2D.Impulse);
-    }
+    private void KnockBack() => _aliveRigidBody2D.AddForce(new Vector2(_knockBackXForce * _playerFacingDirection, _knockBackYForce), ForceMode2D.Impulse);
 
     private void Die()
     {
-        aliveGO.SetActive(false);
-        deadTopGO.SetActive(true);
-        deadBottomGO.SetActive(true);
-        deadShieldGO.SetActive(true);
+        _aliveGO.SetActive(false);
+        _deadTopGO.SetActive(true);
+        _deadBottomGO.SetActive(true);
+        _deadShieldGO.SetActive(true);
 
-        deadTopGO.transform.position = aliveGO.transform.position;
-        deadBottomGO.transform.position = aliveGO.transform.position;
-        deadShieldGO.transform.position = aliveGO.transform.position;
+        _deadTopGO.transform.position = _aliveGO.transform.position;
+        _deadBottomGO.transform.position = _aliveGO.transform.position;
+        _deadShieldGO.transform.position = _aliveGO.transform.position;
 
-        deadTopRigidBody2D.AddForce(new Vector2(deadTopXForce * playerFacingDirection, deadTopYForce), ForceMode2D.Impulse);
-        deadBottomRigidBody2D.AddForce(new Vector2(deadBottomXForce * playerFacingDirection, deadBottomYForce), ForceMode2D.Impulse);
-        deadShieldRigidBody2D.AddForce(new Vector2(deadShieldXForce * playerFacingDirection, deadShieldYForce), ForceMode2D.Impulse);
+        _deadTopRigidBody2D.AddForce(new Vector2(_deadTopXForce * _playerFacingDirection, _deadTopYForce), ForceMode2D.Impulse);
+        _deadBottomRigidBody2D.AddForce(new Vector2(_deadBottomXForce * _playerFacingDirection, _deadBottomYForce), ForceMode2D.Impulse);
+        _deadShieldRigidBody2D.AddForce(new Vector2(_deadShieldXForce * _playerFacingDirection, _deadShieldYForce), ForceMode2D.Impulse);
     }
 }
