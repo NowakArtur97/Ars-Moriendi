747985241a748930c59224448ae5176a1cc60ab0
NowakArtur97 <nowart97@gmail.com>
2020-12-22 08:04:03 +0100

    Create Health Bar script

---
+++ Assets/Player/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private Slider _slider;
+
+    public void SetHealth(int health)
+    {
+        _slider.value = health;
+    }
+
+    public void SetMaxHealth(int maxHealth)
+    {
+        _slider.maxValue = maxHealth;
+        _slider.value = maxHealth;
+    }
+}
