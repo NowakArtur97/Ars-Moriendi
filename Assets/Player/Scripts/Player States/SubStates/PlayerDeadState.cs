72b9942ca5aeb6b129d476f6051ce96837e31c49
NowakArtur97 <nowart97@gmail.com>
2020-12-23 08:08:38 +0100

    Create Player Dead state

--- Assets/Player/Scripts/Player States/SubStates/PlayerDeadState.cs
+++ Assets/Player/Scripts/Player States/SubStates/PlayerDeadState.cs
@@ -1,6 +1,22 @@
-﻿public class PlayerDeadState : PlayerState
+﻿using UnityEngine;
+
+public class PlayerDeadState : PlayerState
 {
-    public PlayerDeadState(Player player, PlayerFiniteStateMachine playerFiniteStateMachine, string animationBoolName) : base(player, playerFiniteStateMachine, animationBoolName)
+    private D_PlayerDeadState _deadStateData;
+
+    public PlayerDeadState(Player player, PlayerFiniteStateMachine playerFiniteStateMachine, string animationBoolName, D_PlayerDeadState deadStateData)
+        : base(player, playerFiniteStateMachine, animationBoolName)
     {
+        _deadStateData = deadStateData;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        GameObject.Instantiate(_deadStateData.deathChunkEffectGO, Player.AliveGameObject.transform.position, _deadStateData.deathChunkEffectGO.transform.rotation);
+        GameObject.Instantiate(_deadStateData.bloodEffectGO, Player.AliveGameObject.transform.position, _deadStateData.bloodEffectGO.transform.rotation);
+
+        GameObject.Destroy(Player);
     }
 }
