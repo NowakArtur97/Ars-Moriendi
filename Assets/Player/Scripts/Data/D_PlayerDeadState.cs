02891929843f96d8504d9808bfc105522d236daa
NowakArtur97 <nowart97@gmail.com>
2020-12-23 07:47:36 +0100

    Create Player Dead State scriptable object

---
+++ Assets/Player/Scripts/Data/D_PlayerDeadState.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player Dead State Data", menuName = "Data/Player State Data/Dead State")]
+public class D_PlayerDeadState : ScriptableObject
+{
+    public GameObject deathChunkEffectGO;
+    public GameObject bloodEffectGO;
+}
