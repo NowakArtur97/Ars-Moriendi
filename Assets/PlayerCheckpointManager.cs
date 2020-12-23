100cda1e389cfaf6e1596400a47b7a9d2dd59e8a
NowakArtur97 <nowart97@gmail.com>
2020-12-23 08:41:58 +0100

    Create Player Checkpoint Manager script

---
+++ Assets/PlayerCheckpointManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheckpointManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _checkpoints;
+    [SerializeField]
+    private GameObject _playerPrefab;
+
+    private Transform _lastCheckpoint;
+
+    private void Start()
+    {
+        _lastCheckpoint = _checkpoints[0];
+        FindObjectOfType<Player>().StatsManager.DeathEvent += OnPlayerDeath;
+    }
+
+    private void OnPlayerDeath() => Instantiate(_playerPrefab, _lastCheckpoint.position, Quaternion.identity);
+}
