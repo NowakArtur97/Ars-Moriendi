d166120e7e33790cc32e4f66572db31855cc4d90
NowakArtur97 <nowart97@gmail.com>
2021-01-08 12:01:52 +0100

    Create Enemy Stats scriptable object

---
+++ Assets/Enemies/Scripts/Data/D_EnemyStats.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy Stats Data", menuName = "Data/Enemy Data/Stats Data")]
+public class D_EnemyStats : ScriptableObject
+{
+    public float maxHealth = 20f;
+
+    public int maxStunResistance = 10;
+    public float stunRecorveryTime = 1.0f;
+}
