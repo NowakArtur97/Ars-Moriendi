f7d32faecc1844e9994aff45e57efb7a11c1f356
NowakArtur97 <nowart97@gmail.com>
2021-02-24 10:26:26 +0100

    Create Ogre Player Detected State

---
+++ Assets/Enemies/Specific Enemies/Ogre/Scripts/Ogre_PlayerDetectedState.cs
@@ -0,0 +1,31 @@
+﻿public class Ogre_PlayerDetectedState : PlayerDetectedState
+{
+    private Ogre _ogre;
+
+    public Ogre_PlayerDetectedState(FiniteStateMachine finiteStateMachine, Enemy entity, string animationBoolName, D_PlayerDetectedState stateData, Ogre ogre)
+        : base(finiteStateMachine, entity, animationBoolName, stateData)
+    {
+        _ogre = ogre;
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (IsAnimationFinished)
+        {
+            if (ShouldPerformCloseRangeAction && IsPlayerInMinAgroRange)
+            {
+                FiniteStateMachine.ChangeState(_ogre.MeleeAttackState);
+            }
+            else if (ShouldPerformLongRangeAction && IsPlayerInMaxAgroRange)
+            {
+                FiniteStateMachine.ChangeState(_ogre.StompAttackState);
+            }
+            else if (!IsPlayerInMaxAgroRange)
+            {
+                FiniteStateMachine.ChangeState(_ogre.IdleState);
+            }
+        }
+    }
+}
