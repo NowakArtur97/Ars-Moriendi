968d7ca579a3853cd00169d91ec6f28931109e9e
NowakArtur97 <nowart97@gmail.com>
2021-02-24 09:59:47 +0100

    Create Ogre Dead State

---
+++ Assets/Enemies/Specific Enemies/Ogre/Scripts/Ogre_DeadState.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Ogre_DeadState : DeadState
+{
+    private Ogre _ogre;
+
+    private EffectsDetails _effectsDetails;
+
+    public Ogre_DeadState(FiniteStateMachine finiteStateMachine, Enemy entity, string animationBoolName, D_DeadState stateData, Ogre ogre)
+        : base(finiteStateMachine, entity, animationBoolName, stateData)
+    {
+        _ogre = ogre;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        _effectsDetails.material = _ogre.MyMaterial;
+        _effectsDetails.activeOnStart = _ogre.DissolveEffectData.activeOnStart;
+        _effectsDetails.startValue = _ogre.DissolveEffectData.startValue;
+        _effectsDetails.propertyName = _ogre.DissolveEffectData.propertyName;
+
+        _ogre.DissolveEffect.SetupEffect(_effectsDetails);
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (IsAnimationFinished && Time.time >= AnimationFinishedTime + _ogre.DissolveEffectData.timeBeforeDissolving)
+        {
+            _ogre.DissolveEffect.StartEffect();
+        }
+    }
+}
