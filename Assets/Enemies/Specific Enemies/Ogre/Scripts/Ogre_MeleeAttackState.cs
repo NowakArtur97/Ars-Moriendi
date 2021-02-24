1a9f84e684e9fa794b468cfce000fe4f83aca25a
NowakArtur97 <nowart97@gmail.com>
2021-02-24 10:28:31 +0100

    Create Ogre Melee Attack State

---
+++ Assets/Enemies/Specific Enemies/Ogre/Scripts/Ogre_MeleeAttackState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Ogre_MeleeAttackState : MeleeAttackState
+{
+    private Ogre _ogre;
+
+    public Ogre_MeleeAttackState(FiniteStateMachine finiteStateMachine, Enemy entity, string animationBoolName, Transform attackPosition,
+        D_MeleeAttackState stateData, Ogre ogre) : base(finiteStateMachine, entity, animationBoolName, attackPosition, stateData)
+    {
+        _ogre = ogre;
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (IsAnimationFinished)
+        {
+            if (IsPlayerInMaxAgroRange)
+            {
+                FiniteStateMachine.ChangeState(_ogre.PlayerDetectedState);
+            }
+            else
+            {
+                FiniteStateMachine.ChangeState(_ogre.LookForPlayerState);
+            }
+        }
+    }
+}
