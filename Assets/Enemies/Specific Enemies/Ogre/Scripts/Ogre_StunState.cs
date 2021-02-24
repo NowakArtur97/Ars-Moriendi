d4a94881f50e5e3992009646738759f7b752c5f7
NowakArtur97 <nowart97@gmail.com>
2021-02-24 10:23:23 +0100

    Create Ogre Stun State

---
+++ Assets/Enemies/Specific Enemies/Ogre/Scripts/Ogre_StunState.cs
@@ -0,0 +1,28 @@
+﻿public class Ogre_StunState : StunState
+{
+    private Ogre _ogre;
+
+    public Ogre_StunState(FiniteStateMachine finiteStateMachine, Enemy enemy, string animationBoolName, D_StunState stateData, Ogre ogre)
+        : base(finiteStateMachine, enemy, animationBoolName, stateData)
+    {
+        _ogre = ogre;
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (IsStunTimeOver)
+        {
+            if (IsPlayerInMaxAgroRange)
+            {
+                FiniteStateMachine.ChangeState(_ogre.PlayerDetectedState);
+            }
+            else
+            {
+                FiniteStateMachine.ChangeState(_ogre.IdleState);
+            }
+        }
+    }
+}
+
