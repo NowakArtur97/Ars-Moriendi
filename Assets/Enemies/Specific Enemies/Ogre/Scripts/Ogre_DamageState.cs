5ec6486d42348dc21a5c6656e89a9eb0f9b601ff
NowakArtur97 <nowart97@gmail.com>
2021-02-24 10:22:13 +0100

    Create Ogre Damage State

---
+++ Assets/Enemies/Specific Enemies/Ogre/Scripts/Ogre_DamageState.cs
@@ -0,0 +1,31 @@
+﻿public class Ogre_DamageState : DamageState
+{
+    private Ogre _ogre;
+
+    public Ogre_DamageState(FiniteStateMachine finiteStateMachine, Enemy enemy, string animationBoolName, D_DamageState stateData, Ogre ogre)
+        : base(finiteStateMachine, enemy, animationBoolName, stateData)
+    {
+        _ogre = ogre;
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (IsAnimationFinished)
+        {
+            if (_ogre.StatsManager.IsStunned && FiniteStateMachine.CurrentState != _ogre.StunState)
+            {
+                FiniteStateMachine.ChangeState(_ogre.StunState);
+            }
+            else if (IsPlayerInMaxAgroRange)
+            {
+                FiniteStateMachine.ChangeState(_ogre.PlayerDetectedState);
+            }
+            else
+            {
+                FiniteStateMachine.ChangeState(_ogre.IdleState);
+            }
+        }
+    }
+}
