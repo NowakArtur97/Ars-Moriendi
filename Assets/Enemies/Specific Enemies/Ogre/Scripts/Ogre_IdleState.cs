8990f10eb449b58c4816caf07970b60a87afd2ab
NowakArtur97 <nowart97@gmail.com>
2021-02-24 09:56:01 +0100

    Create Ogre Idle state

---
+++ Assets/Enemies/Specific Enemies/Ogre/Scripts/Ogre_IdleState.cs
@@ -0,0 +1,26 @@
+﻿public class Ogre_IdleState : IdleState
+{
+    private Ogre _ogre;
+
+    public Ogre_IdleState(FiniteStateMachine finiteStateMachine, Enemy entity, string animationBoolName, D_IdleState stateData, Ogre slime)
+        : base(finiteStateMachine, entity, animationBoolName, stateData)
+    {
+        _ogre = slime;
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (IsPlayerInMaxAgroRange)
+        {
+            ShouldFlipAfterIdle(false);
+            FiniteStateMachine.ChangeState(_ogre.PlayerDetectedState);
+        }
+        else if (IsIdleTimeOver)
+        {
+            ShouldFlipAfterIdle(!IsDetectingLedge || IsDetectingWall);
+            FiniteStateMachine.ChangeState(_ogre.MoveState);
+        }
+    }
+}
