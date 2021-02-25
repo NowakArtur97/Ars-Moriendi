95e024d7c2f5124cc2ad176d7413839e30882481
NowakArtur97 <nowart97@gmail.com>
2021-02-25 07:37:36 +0100

    Create scriptable object for Specific Area Attack state

---
+++ Assets/Enemies/Scripts/Data/D_SpecificAreaAttackState.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "_SpecificAreaAttackStateData", menuName = "Data/Enemy State Data/Specific Area Attack State")]
+public class D_SpecificAreaAttackState : ScriptableObject
+{
+    public GameObject attack;
+    public float attackOffsetX = 2.0f;
+    public float attackOffsetY = 2.0f;
+}
