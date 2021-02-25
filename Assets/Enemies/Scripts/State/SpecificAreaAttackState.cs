f8e525edd1608bf6fcce490a9b40b1ac6c831a0c
NowakArtur97 <nowart97@gmail.com>
2021-02-25 07:38:07 +0100

    Create Specific Area Attack State

---
+++ Assets/Enemies/Scripts/State/SpecificAreaAttackState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SpecificAreaAttackState : AttackState
+{
+    protected D_SpecificAreaAttackState StateData;
+
+    private Vector2 _attackPosition;
+
+    public SpecificAreaAttackState(FiniteStateMachine finiteStateMachine, Enemy enemy, string animationBoolName, Transform attackPosition,
+        D_SpecificAreaAttackState stateData) : base(finiteStateMachine, enemy, animationBoolName, attackPosition)
+    {
+        StateData = stateData;
+    }
+
+    public override void TriggerAttack()
+    {
+        base.TriggerAttack();
+
+        _attackPosition.Set(Enemy.transform.position.x + StateData.attackOffsetX * Enemy.FacingDirection, Enemy.transform.position.y + StateData.attackOffsetY);
+
+        GameObject.Instantiate(StateData.attack, _attackPosition, Quaternion.identity);
+    }
+}
